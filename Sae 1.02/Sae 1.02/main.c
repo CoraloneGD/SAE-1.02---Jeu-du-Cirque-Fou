76365f9d6d67b04a84c75461e76ea6032394290c
Naitoo37 <33103893+Naitoo3@users.noreply.github.com>
2025-12-23 00:00:24 +0100

    Update main.c

--- Sae 1.02/Sae 1.02/main.c
+++ Sae 1.02/Sae 1.02/main.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 #include "joueur.h"
 #include "partie.h"
 
-
+#pragma warning(disable:4996)
+#define _CRT_SECURE_NO_WARNINGS
 
 int main() {
-	Parametres p;
-	InitialisePartie(&p); // On initialise une partie
+	Joueur jo; joueursListe joListe; Parametres p;
+	char commande[256] = "";
+	printf("Ajout \n");
+	do {
+		if (scanf("%255s", commande) != 1) {
+			return 0;
+		}
+		// Ajout joueurs
+		if (strcmp(commande, "Ajouter") == 0) {
+			AjoutJoueurs(&jo, &joListe);
+		}
+		// Saisie des noms des joueurs
+		
+		
+		
+
+
+	} while (1);
 
 }
