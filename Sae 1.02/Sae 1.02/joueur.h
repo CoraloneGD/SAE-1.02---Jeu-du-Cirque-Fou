18b32833f883e4aeec7d5ba582981779bf14245b
Naitoo37 <33103893+Naitoo3@users.noreply.github.com>
2025-12-23 00:02:56 +0100

    Update joueur.h

--- Sae 1.02/Sae 1.02/joueur.h
+++ Sae 1.02/Sae 1.02/joueur.h
@@ -1,42 +1,44 @@
 #pragma once
 
-#define MAX_CHAR 30 // Nombre de caractères maximum pour le nom d'un joueur.
+#define MAX_CHAR 31 // Nombre de caractÃ¨res maximum pour le nom d'un joueur.
 #define MAX_JOUEURS 10 // Nombre maximum de joueurs dans une partie.
 
 //enum {
 //	KI, // Animal du haut - podium bleu -> saute sur sommet podium rouge
 //	LO, // Animal du haut - podium rouge -> saute sur sommet podium bleu
-//	SO, // Deux animaux du sommet -> Échange de places
+//	SO, // Deux animaux du sommet -> Ã‰change de places
 //	NI, // Animal du bas - podium bleu -> va en haut du podium bleu
 //	MA  // Animal du bas - podium rouge -> va en haut du podium rouge
 //};
 
 /**
-	@brief La structure représentant un joueur.
+	@brief La structure reprÃ©sentant un joueur.
 	@param[in] nom Le nom du joueur.
 	@param[in, out] score Le score actuel du joueur.
-	@param[in] prochainTour une variable qui aura une fonction de booléen indiquant si le joueur peut rejouer ou non.
+	@param[in] prochainTour une variable qui aura une fonction de boolÃ©en indiquant si le joueur peut rejouer ou non.
 
 	@sa MAX_CHAR
 */
 typedef struct {
 	char nom[MAX_CHAR];
+	char prenom[MAX_CHAR];
 	int score;     
 	int prochainTour;
 } Joueur;
 
 
 /**
-	@brief La structure représentant une liste de joueurs.
+	@brief La structure reprÃ©sentant une liste de joueurs.
 	@param[in] id L'identifiant unique de la liste de joueurs.
 	@param[in, out] listeJoueurs Le tableau contenant les joueurs.
 	@sa MAX_JOUEURS
 */
 typedef struct {
 	int id;
-	char prenom;
 	Joueur listeJoueurs[MAX_JOUEURS];
 } joueursListe;
+
+int AjoutJoueurs(Joueur* jo, joueursListe* joListe);
 
 
 
