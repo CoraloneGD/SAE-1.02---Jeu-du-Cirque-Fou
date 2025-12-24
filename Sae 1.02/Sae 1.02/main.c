1ecb96738848783048b7efab3f0ef4779a6fbbcd
Naitoo37 <33103893+Naitoo3@users.noreply.github.com>
2025-12-24 23:54:45 +0100

    Update main.c

--- Sae 1.02/Sae 1.02/main.c
+++ Sae 1.02/Sae 1.02/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <assert.h>
+#include <stdlib.h>
 #include <string.h>
 #include "joueur.h"
 #include "partie.h"
@@ -7,24 +7,38 @@
 #pragma warning(disable:4996)
 #define _CRT_SECURE_NO_WARNINGS
 
-int main() {
-	Joueur jo; joueursListe joListe; Parametres p;
-	char commande[256] = "";
-	printf("Ajout \n");
-	do {
-		if (scanf("%255s", commande) != 1) {
-			return 0;
+int main(int argc, char* ListeJoueurs[]) {
+	if (argc < 3) {
+		printf("Il faut au moins deux joueurs pour lancer une partie !\n");
+		return EXIT_FAILURE;
+	}
+
+	for (int i = 1; i < argc; i++) {
+		for (int j = i + 1; j < argc; j++) {
+			if (strcmp(ListeJoueurs[i], ListeJoueurs[j]) == 0) {
+				printf("Les noms doivent etres distincts\n");
+				return EXIT_FAILURE;
+			}
 		}
-		// Ajout joueurs
-		if (strcmp(commande, "Ajouter") == 0) {
-			AjoutJoueurs(&jo, &joListe);
-		}
-		// Saisie des noms des joueurs
-		
-		
-		
+	}
+
+	// Ajoute les joueurs dans la liste.
+	joueursListe liste = { .taille = 0 };
+	for (int i = 1; i < argc; i++) {
+		Joueur joueur;
+		strncpy(joueur.prenom, ListeJoueurs[i], MAX_CHAR);
+		joueur.prenom[MAX_CHAR - 1] = '\0';
+		joueur.score = 0;
+		joueur.prochainTour = 0;
+		liste.listeJoueurs[liste.taille++] = joueur;
+		printf("Joueur ajoute: %s\n", joueur.prenom);
+	}
 
 
-	} while (1);
+	// Affichage des ordres possibles selon crazy.cfg
+	Parametres p = { 0 };
+	InitialisePartie(&p);
+
+	printf("Voici les ordres possibles: %s \n", p.ordres);
 
 }
